52d795e9b62e3b43b715431d043ce0fb8bc73655
Tathagat Dwivedi <tathagat_dwivedi@hotmail.com>
2018-05-19 15:16:00 -0700

    Testing

---
+++ KnockOutJsMvcCreateArticle/Models/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockOutJsMvcCreateArticle.Models
+{
+    public class Property
+    {
+        
+            public string Address { get; set; }
+            public string APN { get; set; }
+            public string Zip { get; set; }
+        
+    }
+}
---
+++ KnockOutJsMvcCreateArticle/Utility/pdfConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Xsl;
+using System.Text;
+using System.Xml;
+
+
+namespace KnockOutJsMvcCreateArticle.Utility
+{
+    public class pdfConverter
+    {
+
+        // Getting file path     
+    public String xmlToPdf(string xmlpath,string xslpath)
+        {
+            //string strXSLTFile = "Content/Files/XSLTFile1.xslt";
+            //string strXMLFile = "Content/Files/XMLFile1.xml";
+            string strXSLTFile = xslpath;
+            string strXMLFile = xmlpath;
+
+            // Creating XSLCompiled object     
+            XslCompiledTransform objXSLTransform = new XslCompiledTransform();
+            objXSLTransform.Load(strXSLTFile);
+            // Execute the transform and output the results to a file.
+            objXSLTransform.Transform(strXMLFile, @"\Projects\KnockOutJsMvcCreateArticle\KnockOutJsMvcCreateArticle\Content\Files\XMLFile1.html");
+        string htmlfile ="Content/Files/XMLFile1.html";
+        return htmlfile;
+        }
+        
+    }
+}
